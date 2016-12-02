c9ada67029a9db7afa24a5268e368070da8bb173
orri14 <orri14@ru.is>
2016-12-02 20:33:20 +0000

    minor

--- MovieSearch/MovieSearch.iOS/Controllers/MovieController.cs
+++ MovieSearch/MovieSearch.iOS/Controllers/MovieController.cs
@@ -92,7 +92,7 @@ namespace MovieSearch.iOS.Controllers
         {
             UILabel label = new UILabel()
             {
-                Frame = new CGRect(HorizontalMargin, 230, this.View.Bounds.Width - HorizontalMargin * 2, 200),
+                Frame = new CGRect(HorizontalMargin, _yCoord + 100, this.View.Bounds.Width - HorizontalMargin * 2, 200),
                 Text = _movieInfo.description,
                 LineBreakMode = UILineBreakMode.WordWrap,
                 Lines = 8,
