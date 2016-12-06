5c06f5ec9504b8bf710e666a025935577c72cb18
orri14 <orri14@ru.is>
2016-12-06 14:35:29 +0000

    Adds a skelly for .droid

---
+++ MovieSearch/MovieSearch.Droid/MovieListActivity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+
+namespace MovieSearch.Droid
+{
+    
+    
+    [Activity(Theme = "@style/MyTheme", Label = "Movie list")]
+    public class MovieListActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var jsonStr = this.Intent.GetStringExtra("movieList");
+            var movieList = JsonConvert.DeserializeObject<List<FilmInfo>>(jsonStr);
+            this.ListAdapter = new MovieListAdapter(this, movieList);
+            // Create your application here
+        }
+    }
+}
---
+++ MovieSearch/MovieSearch.Droid/MovieListAdapter.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MovieSearch.Model;
+
+
+namespace MovieSearch.Droid
+{
+    
+    public class MovieListAdapter : BaseAdapter<FilmInfo>
+    {
+        private Activity _context;
+
+        private List<FilmInfo> _movieList;
+
+        public MovieListAdapter(Activity context, List<FilmInfo> movieList)
+        {
+            this._context = context;
+            this._movieList = movieList;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            if (view == null)
+            {
+                view = this._context.LayoutInflater.Inflate(Resource.Layout.MovieListItem, null);
+            }
+
+            var person = this._movieList[position];
+            view.FindViewById<TextView>(Resource.Id.name).Text = person.Name;
+            view.FindViewById<TextView>(Resource.Id.year).Text = person.BirthYear.ToString();
+
+            var resourceId = this._context.Resources.GetIdentifier(
+                person.ImageName,
+                "drawable",
+                this._context.PackageName);
+            view.FindViewById<ImageView>(Resource.Id.picture).SetBackgroundResource(resourceId);
+
+            return view;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return this._movieList.Count;
+            }
+        }
+
+        public override FilmInfo this[int position]
+        {
+            get
+            {
+                return this._movieList[position];
+            }
+        }
+    }
+}
